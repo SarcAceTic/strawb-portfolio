d272146d286aafd888face4617c6ec08e1292374
SarcAceTic <sarcacetic@gmail.com>
2023-12-13 20:00:12 +0700

    Added a footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <v-app>
     <Header />
     <v-main class="mt-4"><RouterView /></v-main>
+    <Footer />
   </v-app>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <v-footer class="pa-2 d-flex flex-column text-grey">
+    <p>
+      Made with
+      <span class="text-pink-lighten-1">
+        Love
+        <v-icon>mdi-heart</v-icon>
+      </span>
+    </p>
+    <p class="mt-2">
+      Made by SarcAceTic -
+      <a class="text-purple-lighten-1" href="https://sarcacetic.vercel.app" target="_blank">
+        My Website
+        <v-icon>mdi-arrow-top-right</v-icon>
+      </a>
+    </p>
+    <p class="mt-5">
+      This website is under the
+      <b>MIT License</b>
+      . You can take a look at the repository here:
+      <a href="https://github.com/SarcAceTic/strawb-portfolio/" target="_blank">
+        <v-icon>mdi-github</v-icon>
+        Repository
+      </a>
+    </p>
+  </v-footer>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
