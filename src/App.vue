29fb96a92e7a809dd2a95f7baa9c3851bd0cc5c8
SarcAceTic <sarcacetic@gmail.com>
2023-12-13 15:38:18 +0700

    Added header navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
 </script>
 
 <template>
   <v-app>
+    <Header />
     <v-main><RouterView /></v-main>
   </v-app>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,1 +1,17 @@
 <script setup></script>
+
+<template>
+  <v-app-bar class="pa-2" elevation="4">
+    <v-app-bar-title>
+      <div class="d-flex align-center">
+        <v-img max-height="35" max-width="35" src="/strawberryicon.png"></v-img>
+        <h3 class="ml-2 mr-1">Strawberry</h3>
+        <h3 class="text-purple">Chae</h3>
+      </div>
+    </v-app-bar-title>
+    <v-btn class="mx-2">Home</v-btn>
+    <v-btn class="mx-2">About</v-btn>
+    <v-btn class="mx-2">My Artworks</v-btn>
+    <v-btn class="mx-2">Contacts</v-btn>
+  </v-app-bar>
+</template>
