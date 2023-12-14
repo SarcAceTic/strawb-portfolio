4746a906879062fa1573b90fba86a73ae6aaee99
SarcAceTic <sarcacetic@gmail.com>
2023-12-14 19:15:10 +0700

    Added a navigation drawer for mobile sized viewports

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+let navDrawer = ref(false)
+</script>
 
 <template>
   <v-app-bar class="pa-2" elevation="4">
@@ -12,6 +16,36 @@
     <v-btn class="d-none d-md-flex mx-2">Home</v-btn>
     <v-btn class="d-none d-md-flex mx-2">About</v-btn>
     <v-btn class="d-none d-md-flex mx-2">My Artworks</v-btn>
-    <v-app-bar-nav-icon class="d-flex d-md-none"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      @click.stop="navDrawer = !navDrawer"
+      class="d-flex d-md-none"></v-app-bar-nav-icon>
   </v-app-bar>
+  <v-navigation-drawer location="right" v-model="navDrawer">
+    <template v-slot:image>
+      <v-img class="sass h-100" src="/someimage.jpg"></v-img>
+    </template>
+    <v-list class="mt-4">
+      <v-list-item prepend-icon="mdi-home" link>
+        <p class="text-button">
+          home
+        </p>
+      </v-list-item>
+      <v-list-item prepend-icon="mdi-account" link>
+        <p class="text-button">
+          about me
+        </p>
+      </v-list-item>
+      <v-list-item prepend-icon="mdi-palette" link>
+        <p class="text-button">
+          my artworks
+        </p>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
+
+<style scoped>
+.sass {
+ opacity: 0.4;
+}
+</style>
